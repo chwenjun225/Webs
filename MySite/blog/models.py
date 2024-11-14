c5d305db9e748bf1429e79b4e633a3bcc863c1c6
chwenjun225 <trantuan22052k@gmail.com>
2024-11-14 15:56:03 +0800

    Refactor blog models for improved readability and consistency

--- MySite/blog/models.py
+++ MySite/blog/models.py
@@ -4,11 +4,7 @@ from django.utils import timezone
 
 class PublishedManager(models.Manager):
     def get_queryset(self):
-        return (
-            super().get_queryset().filter(
-                status=Post.Status.PUBLISHED, 
-            )
-        )
+        return (super().get_queryset().filter(status=Post.Status.PUBLISHED))
 
 class Post(models.Model):
     class Status(models.TextChoices):
@@ -16,26 +12,18 @@ class Post(models.Model):
         PUBLISHED = 'PB', 'Published'
     title = models.CharField(max_length=250)
     slug = models.SlugField(max_length=250, allow_unicode=True)
-    author = models.ForeignKey(
-        settings.AUTH_USER_MODEL,
-        on_delete=models.CASCADE,
-        related_name='blog_posts', 
-    )
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='blog_posts')
     body = models.TextField()
     publish = models.DateTimeField(default=timezone.now)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
-    status = models.CharField(
-        max_length=2,
-        choices=Status.choices, 
-        default=Status.DRAFT, 
-    )
+    status = models.CharField(max_length=2, choices=Status.choices, default=Status.DRAFT)
     objects = models.Manager() # The default manager 
     published = PublishedManager() # Our custom manager 
+
     class Meta:
         ordering = ['-publish']
-        indexes = [
-            models.Index(fields=['-publish']), 
-        ]
+        indexes = [models.Index(fields=['-publish'])]
+        
     def __str__(self):
         return self.title
