07e3d0f51fac181ed646dbee1df6eb3a8ad65918
chwenjun225 <trantuan22052k@gmail.com>
2024-11-15 05:02:22 +0800

    Add post sharing functionality with email support; update templates and URLs

--- MySite/blog/views.py
+++ MySite/blog/views.py
@@ -1,7 +1,48 @@
-from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger, UnorderedObjectListWarning, InvalidPage
-from django.shortcuts import render, get_object_or_404
-from .models import Post 
 from django.views.generic import ListView
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger, UnorderedObjectListWarning, InvalidPage
+from django.core.mail import send_mail
+from django.shortcuts import render, get_object_or_404
+
+from .models import Post 
+from .forms import EmailPostForm
+
+def post_share(request, post_id):
+	# Retrieve post by id
+	post = get_object_or_404(
+		Post,
+		id=post_id,
+		status=Post.Status.PUBLISHED
+	)
+	sent = False
+	if request.method == 'POST':
+		# Form was submitted
+		form = EmailPostForm(request.POST)
+		if form.is_valid():
+			# Form fields passed validation
+			cd = form.cleaned_data
+			# ... send email
+			post_url = request.build_absolute_uri(post.get_absolute_url())
+			subject = (
+				f"{cd['name']} ({cd['email']}) "
+				f"recommends you read {post.title}"
+			)
+			message = (
+				f"Read {post.title} at {post_url}\n\n"
+				f"{cd['name']}\'s comments: {cd['comments']}"
+			)
+			send_mail(subject=subject, message=message, from_email=None, recipient_list=[cd['to']])
+			sent = True
+	else:
+		form = EmailPostForm()
+	return render(
+		request,
+		'blog/post/share.html',
+		{
+			'post': post,
+			'form': form, 
+			'sent': sent
+		}
+	)
 
 def post_list(request):
 	post_list = Post.published.all()
@@ -11,10 +52,10 @@ def post_list(request):
 	try:
 		posts = paginator.page(page_number)
 	except EmptyPage:
-        # If page_number is out of range get last page of results
+		# If page_number is out of range get last page of results
 		posts = paginator.page(paginator.num_pages)
 	except PageNotAnInteger or UnorderedObjectListWarning or InvalidPage:
-        # If page_number is not an integer, warning, invalid_page -> get the first page
+		# If page_number is not an integer, warning, invalid_page -> get the first page
 		posts = paginator.page(1)
 	return render(request, 'blog/post/list.html', {'posts': posts})
 
@@ -31,9 +72,9 @@ def post_detail(request, id, year, month, day, post):
 	return render(request, 'blog/post/detail.html', {'post': post})
 
 class PostListView(ListView):
-    """Alternative post list view."""
-    queryset = Post.published.all()
-    context_object_name = 'posts'
-    paginate_by = 3
-    template_name = 'blog/post/list.html'
+	"""Alternative post list view."""
+	queryset = Post.published.all()
+	context_object_name = 'posts'
+	paginate_by = 3
+	template_name = 'blog/post/list.html'
 	
--- MySite/mysite/settings.py
+++ MySite/mysite/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -122,3 +123,18 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Email server configuration
+# EMAIL_HOST_USER='test@gmail.com'
+# EMAIL_HOST_PASSWORD='lldb ksar zlwt elpx'
+# DEFAULT_FROM_EMAIL='My Blog <trantuan22052k@gmail.com>'
+
+# EMAIL_HOST = 'smtp.gmail.com'
+# EMAIL_HOST_USER = config('EMAIL_HOST_USER')
+# EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
+# EMAIL_PORT = 587
+# EMAIL_USE_TLS = True
+# DEFAULT_FROM_EMAIL = config('DEFAULT_FROM_EMAIL')
+
+# Send emails via SMTP
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
