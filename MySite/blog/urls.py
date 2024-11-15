be638f30cebc4939adf61e2118726c0fa12fe64f
chwenjun225 <trantuan22052k@gmail.com>
2024-11-15 14:56:09 +0800

    Enhance blog post detail and list templates with similar posts and comment functionality; update email share form and improve tag display

--- MySite/blog/urls.py
+++ MySite/blog/urls.py
@@ -5,10 +5,6 @@ urlpatterns = [
     # post views
     # path('', views.post_list, name='post_list'),
     path('', views.PostListView.as_view(), name='post_list'),
-    path(
-        '<int:year>/<int:month>/<int:day>/<slug:post>/', 
-        views.post_detail, 
-        name='post_detail'
-    ),
+    path('<int:year>/<int:month>/<int:day>/<slug:post>/', views.post_detail, name='post_detail'),
     path('<int:post_id>/share/', views.post_share, name='post_share'),
 ]
--- MySite/mysite/settings.py
+++ MySite/mysite/settings.py
@@ -123,3 +123,12 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Email server configuration
+# EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+# EMAIL_HOST = 'smtp.gmail.com'
+# EMAIL_HOST_USER = config('EMAIL_HOST_USER')
+# EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
+# EMAIL_PORT = 587
+# EMAIL_USE_TLS = True
+# DEFAULT_FROM_EMAIL = config('DEFAULT_FROM_EMAIL')
