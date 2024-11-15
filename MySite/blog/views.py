4c8ac4495fc8740a2698453c7e2f7c1e838801e4
chwenjun225 <trantuan22052k@gmail.com>
2024-11-16 01:20:36 +0800

    Add custom template tags for total posts and latest posts; update base template and post detail view

--- MySite/blog/views.py
+++ MySite/blog/views.py
@@ -85,7 +85,11 @@ def post_detail(request, id, year, month, day, post):
 	post_tags_ids = post.tags.values_list('id', flat=True)
 	similar_posts = Post.published.filter(tags__in=post_tags_ids).exclude(id=post.id)
 	similar_posts = similar_posts.annotate(same_tags=Count('tags')).order_by('-same_tags', '-publish')[:4]
-	return render(request, 'blog/post/detail.html', {'post': post, 'comments': comments, 'form': form, 'similar_posts': similar_posts})
+	return render(
+		request, 
+		'blog/post/detail.html', 
+		{'post': post, 'comments': comments, 'form': form, 'similar_posts': similar_posts}
+	)
 
 class PostListView(ListView):
 	"""Alternative post list view."""
