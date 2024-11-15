8878a9c6a6454f4c29f7bfc0a4524719db2da140
chwenjun225 <trantuan22052k@gmail.com>
2024-11-16 01:38:52 +0800

    Add latest and most commented posts features to the blog sidebar; update base template and create corresponding template tags

--- MySite/blog/templatetags/blogs_tag.py
+++ MySite/blog/templatetags/blogs_tag.py
@@ -1,3 +1,4 @@
+from django.db.models import Count
 from django import template
 from ..models import Post
 
@@ -10,3 +11,7 @@ def total_posts():
 def show_latest_posts(count=5):
 	latest_posts = Post.published.order_by('-publish')[:count]
 	return {'latest_posts': latest_posts}
+
+@register.simple_tag
+def get_most_commented_posts(count=5):
+    return Post.published.annotate(total_comments=Count('comments')).order_by('-total_comments')[:count]
