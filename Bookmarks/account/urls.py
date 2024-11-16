42e5cc47b320b5648012f75ea08c42dd45ddb77b
chwenjun225 <trantuan22052k@gmail.com>
2024-11-16 17:55:07 +0800

    Implement user authentication; add login and dashboard views, forms, and templates

---
+++ Bookmarks/account/urls.py
@@ -0,0 +1,11 @@
+from django.contrib.auth import views as auth_views
+from django.urls import path
+from . import views
+urlpatterns = [
+    # previous login url
+    # path('login/', views.user_login, name='login'),
+    # login / logout urls
+    path('login/', auth_views.LoginView.as_view(), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('', views.dashboard, name='dashboard'),
+]
