cc13e32e866cae11a3bbfd24384ebab2f6999acf
chwenjun225 <trantuan22052k@gmail.com>
2024-11-18 23:55:33 +0800

    Add image detail and list views; implement like functionality and AJAX support

--- Bookmarks/bookmarks/settings.py
+++ Bookmarks/bookmarks/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'social_django', 
     'django_extensions', 
     'images.apps.ImagesConfig',
+    'easy_thumbnails', 
 ]
 
 MIDDLEWARE = [
@@ -155,3 +156,8 @@ SOCIAL_AUTH_PIPELINE = [
     'social_core.pipeline.social_auth.load_extra_data',
     'social_core.pipeline.user.user_details',
 ]
+
+if DEBUG:
+    import mimetypes
+    mimetypes.add_type('application/javascript', '.js', True)
+    mimetypes.add_type('text/css', '.css', True)
--- Bookmarks/images/models.py
+++ Bookmarks/images/models.py
@@ -1,6 +1,7 @@
 from django.conf import settings
 from django.db import models
 from django.utils.text import slugify
+from django.urls import reverse
 
 class Image(models.Model):
 	user = models.ForeignKey(
@@ -31,4 +32,6 @@ class Image(models.Model):
 		if not self.slug:
 			self.slug = slugify(self.title)
 		super().save(*args, **kwargs)
+	def get_absolute_url(self):
+		return reverse('images:detail', args=[self.id, self.slug])
 	
