609bfc6e0e38e57ef47f5905d098a2c943807b87
chwenjun225 <trantuan22052k@gmail.com>
2024-11-19 23:08:31 +0800

    Remove cart and orders app files; delete unused models, views, forms, and templates

--- MyShop/myshop/urls.py
+++ MyShop/myshop/urls.py
@@ -1,8 +1,7 @@
-"""
-URL configuration for myshop project.
+"""myshop URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.2/topics/http/urls/
+    https://docs.djangoproject.com/en/4.1/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -14,20 +13,9 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.conf import settings
-from django.conf.urls.static import static 
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('cart/', include('cart.urls', namespace='cart')),
-    path('orders/', include('orders.urls', namespace='orders')), 
-    path('', include('shop.urls', namespace='shop')), 
 ]
-
-if settings.DEBUG:
-    urlpatterns += static(
-        settings.MEDIA_URL, 
-        document_root=settings.MEDIA_ROOT, 
-    )
--- MyShop/shop/admin.py
+++ MyShop/shop/admin.py
@@ -1,21 +1,22 @@
-from django.contrib import admin 
-from .models import Category, Product 
+from django.contrib import admin
+from .models import Category, Product
 
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
     list_display = ['name', 'slug']
-    prepopulated_fields = {'slug': ('name', ), }
+    prepopulated_fields = {'slug': ('name',)}
 
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
     list_display = [
-        'name', 
-        'slug', 
-        'price', 
-        'available', 
-        'created', 
-        'updated', 
+        'name',
+        'slug',
+        'price',
+        'available',
+        'created',
+        'updated'
     ]
     list_filter = ['available', 'created', 'updated']
     list_editable = ['price', 'available']
-    prepopulated_fields = {'slug': ('name', ), }
+    prepopulated_fields = {'slug': ('name',)}
+    
