b9afa02a8602f2a0d617bc9e40b0adcdfa2054d1
chwenjun225 <trantuan22052k@gmail.com>
2024-11-20 00:03:37 +0800

    Add cart functionality; implement cart detail view, context processor, and templates

--- MyShop/shop/views.py
+++ MyShop/shop/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import get_object_or_404, render
+
+from cart.forms import CartAddProductForm
+
 from .models import Category, Product
 
 def product_list(request, category_slug=None):
@@ -22,8 +25,12 @@ def product_detail(request, id, slug):
     product = get_object_or_404(
         Product, id=id, slug=slug, available=True
     )
+    cart_product_form = CartAddProductForm()
     return render(
         request,
         'shop/product/detail.html',
-        {'product': product}
+        {
+            'product': product, 
+            'cart_product_form': cart_product_form, 
+        }
     )
