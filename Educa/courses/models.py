e45b3a2e7178fbb86a98ccba1a2af64f519589bf
chwenjun225 <trantuan22052k@gmail.com>
2024-11-22 01:02:35 +0800

    Add content management models; implement Content, ItemBase, and media types for course modules

--- Educa/courses/models.py
+++ Educa/courses/models.py
@@ -1,3 +1,5 @@
+from django.contrib.contenttypes.fields import GenericForeignKey
+from django.contrib.contenttypes.models import ContentType
 from django.contrib.auth.models import User
 from django.db import models
 
@@ -8,6 +10,7 @@ class Subject(models.Model):
 		ordering = ['title']
 	def __str__(self):
 		return self.title
+	
 class Course(models.Model):
 	owner = models.ForeignKey(
 		User,
@@ -27,6 +30,7 @@ class Course(models.Model):
 		ordering = ['-created']
 	def __str__(self):
 		return self.title
+	
 class Module(models.Model):
 	course = models.ForeignKey(
 		Course, related_name='modules', on_delete=models.CASCADE
@@ -35,3 +39,44 @@ class Module(models.Model):
 	description = models.TextField(blank=True)
 	def __str__(self):
 		return self.title
+	
+class Content(models.Model):
+	module = models.ForeignKey(
+		Module,
+		related_name='contents',
+		on_delete=models.CASCADE
+	)
+	content_type = models.ForeignKey(
+		ContentType,
+		on_delete=models.CASCADE, 
+		limit_choices_to={
+            'model__in':('text', 'video', 'image', 'file')
+        }
+	)
+	object_id = models.PositiveIntegerField()
+	item = GenericForeignKey('content_type', 'object_id')	
+
+class ItemBase(models.Model):
+	owner = models.ForeignKey(User,
+		related_name='%(class)s_related',
+		on_delete=models.CASCADE
+	)
+	title = models.CharField(max_length=250)
+	created = models.DateTimeField(auto_now_add=True)
+	updated = models.DateTimeField(auto_now=True)
+	class Meta:
+		abstract = True
+	def __str__(self):
+		return self.title
+	
+class Text(ItemBase):
+	content = models.TextField()
+
+class File(ItemBase):
+	file = models.FileField(upload_to='files')
+
+class Image(ItemBase):
+	file = models.FileField(upload_to='images')
+
+class Video(ItemBase):
+	url = models.URLField()
