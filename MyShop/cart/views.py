d76e0137cad9ee2fe2169068b18ff72205f28769
chwenjun225 <trantuan22052k@gmail.com>
2024-11-21 13:10:41 +0800

    Add product recommendations feature; implement Redis-based recommender and display recommended products in cart and product detail templates

--- MyShop/cart/views.py
+++ MyShop/cart/views.py
@@ -3,6 +3,7 @@ from django.views.decorators.http import require_POST
 
 from coupons.forms import CouponApplyForm
 from shop.models import Product
+from shop.recommender import Recommender
 
 from .cart import Cart
 from .forms import CartAddProductForm
@@ -35,10 +36,18 @@ def cart_detail(request):
 			initial={'quantity': item['quantity'], 'override': True}
 		)
 	coupon_apply_form = CouponApplyForm()
-	return render(request, 'cart/detail.html', {
-		'cart': cart, 
-		'coupon_apply_form': coupon_apply_form, 
+	r = Recommender()
+	cart_products = [item['product'] for item in cart]
+	if cart_products:
+		recommended_products = r.suggest_products_for(cart_products, max_results=4)
+	else:
+		recommended_products = []
+	return render(
+		request, 
+		'cart/detail.html', 
+		{
+			'cart': cart, 
+			'coupon_apply_form': coupon_apply_form, 
+			'recommended_products': recommended_products
 		}
 	)
-
-# TODO: https://learning.oreilly.com/library/view/django-5-by/9781805125457/Text/Chapter_10.xhtml#_idParaDest-289
--- MyShop/payment/webhooks.py
+++ MyShop/payment/webhooks.py
@@ -4,20 +4,18 @@ from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
 
 from orders.models import Order
+from shop.models import Product
+from shop.recommender import Recommender
 
 from .tasks import payment_completed
 
-
 @csrf_exempt
-
 def stripe_webhook(request):
 	payload = request.body
 	sig_header = request.META['HTTP_STRIPE_SIGNATURE']
 	event = None
 	try:
-		event = stripe.Webhook.construct_event(
-			payload, sig_header, settings.STRIPE_WEBHOOK_SECRET
-		)
+		event = stripe.Webhook.construct_event(payload, sig_header, settings.STRIPE_WEBHOOK_SECRET)
 	except ValueError as e:
 		# Invalid payload
 		return HttpResponse(status=400)
@@ -26,14 +24,9 @@ def stripe_webhook(request):
 		return HttpResponse(status=400)
 	if event.type == 'checkout.session.completed':
 		session = event.data.object
-		if (
-			session.mode == 'payment'
-			and session.payment_status == 'paid'
-		):
+		if (session.mode == 'payment'and session.payment_status == 'paid'):
 			try:
-				order = Order.objects.get(
-					id=session.client_reference_id
-				)
+				order = Order.objects.get(id=session.client_reference_id)
 			except Order.DoesNotExist:
 				return HttpResponse(status=404)
 			# mark order as paid
@@ -41,8 +34,11 @@ def stripe_webhook(request):
 			# store Stripe payment ID
 			order.stripe_id = session.payment_intent
 			order.save()
+			# save items bought for product recommendations
+			product_ids = order.items.values_list('product_id')
+			products = Product.objects.filter(id__in=product_ids)
+			r = Recommender()
+			r.products_bought(products)
 			# launch asynchronous task
 			payment_completed.delay(order.id)
 	return HttpResponse(status=200)
-
-# TODO: https://learning.oreilly.com/library/view/django-5-by/9781805125457/Text/Chapter_10.xhtml#_idParaDest-284
