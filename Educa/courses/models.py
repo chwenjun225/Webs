74abb1c3a4134e1494f1b90eb4bc93d0903f670d
chwenjun225 <trantuan22052k@gmail.com>
2024-11-22 01:11:55 +0800

    Add ordering support for Course and Content models; implement OrderField and update templates for user authentication

--- Educa/courses/models.py
+++ Educa/courses/models.py
@@ -3,6 +3,8 @@ from django.contrib.contenttypes.models import ContentType
 from django.contrib.auth.models import User
 from django.db import models
 
+from .fields import OrderField
+
 class Subject(models.Model):
 	title = models.CharField(max_length=200)
 	slug = models.SlugField(max_length=200, unique=True)
@@ -32,29 +34,23 @@ class Course(models.Model):
 		return self.title
 	
 class Module(models.Model):
-	course = models.ForeignKey(
-		Course, related_name='modules', on_delete=models.CASCADE
-	)
+	course = models.ForeignKey(Course, related_name='modules', on_delete=models.CASCADE)
 	title = models.CharField(max_length=200)
 	description = models.TextField(blank=True)
+	order = OrderField(blank=True, for_fields=['course'])
+	class Meta:
+		ordering = ['order']
 	def __str__(self):
-		return self.title
+		return f'{self.order}. {self.title}'
 	
 class Content(models.Model):
-	module = models.ForeignKey(
-		Module,
-		related_name='contents',
-		on_delete=models.CASCADE
-	)
-	content_type = models.ForeignKey(
-		ContentType,
-		on_delete=models.CASCADE, 
-		limit_choices_to={
-            'model__in':('text', 'video', 'image', 'file')
-        }
-	)
+	module = models.ForeignKey(Module, related_name='contents', on_delete=models.CASCADE)
+	content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE, limit_choices_to={'model__in':('text', 'video', 'image', 'file')})
 	object_id = models.PositiveIntegerField()
 	item = GenericForeignKey('content_type', 'object_id')	
+	order = OrderField(blank=True, for_fields=['module'])
+	class Meta:
+		ordering = ['order']
 
 class ItemBase(models.Model):
 	owner = models.ForeignKey(User,
