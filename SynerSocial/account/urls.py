8822240b6a15d5e5bc1ee8a848457a507a351f8d
chwenjun225 <trantuan22052k@gmail.com>
2024-11-25 17:13:50 +0800

    Add profile editing functionality; update dashboard template and URLs, and implement user edit view

--- SynerSocial/account/urls.py
+++ SynerSocial/account/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('', include('django.contrib.auth.urls')),
     path('', views.dashboard, name='dashboard'),
     path('register/', views.register, name='register'),
+    path('edit/', views.edit, name='edit'),
 ]
--- SynerSocial/account/views.py
+++ SynerSocial/account/views.py
@@ -3,7 +3,12 @@ from django.contrib.auth import authenticate, login
 from django.http import HttpResponse
 from django.shortcuts import render
 
-from .forms import LoginForm, UserRegistrationForm
+from .forms import (
+	LoginForm, 
+	UserRegistrationForm, 
+	UserEditForm, 
+	ProfileEditForm
+)
 from .models import Profile
 
 def user_login(request):
@@ -61,4 +66,31 @@ def register(request):
 		request,
 		'account/register.html',
 		{'user_form': user_form}
+	)
+
+@login_required
+def edit(request):
+	if request.method == 'POST':
+		user_form = UserEditForm(
+			instance=request.user,
+			data=request.POST
+		)
+		profile_form = ProfileEditForm(
+			instance=request.user.profile,
+			data=request.POST,
+			files=request.FILES
+		)
+		if user_form.is_valid() and profile_form.is_valid():
+			user_form.save()
+			profile_form.save()
+	else:
+		user_form = UserEditForm(instance=request.user)
+		profile_form = ProfileEditForm(instance=request.user.profile)
+	return render(
+		request,
+		'account/edit.html',
+		{
+			'user_form': user_form,
+			'profile_form': profile_form
+		}
 	)
