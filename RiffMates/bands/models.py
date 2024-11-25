7667ac9dbabd8a0df77e96d4f1cfff8e5695fb87
chwenjun225 <trantuan22052k@gmail.com>
2024-11-26 00:23:35 +0800

    Add Venue and Room models; implement musician listing with pagination and create musicians template

--- RiffMates/bands/models.py
+++ RiffMates/bands/models.py
@@ -7,3 +7,17 @@ class Musician(models.Model):
 
 	def __str__(self):
 		return f"Musician(id={self.id}, last_name={self.last_name})"
+	
+class Venue(models.Model):
+	name = models.CharField(max_length=20)
+
+	def __str__(self):
+		return f"Venue(id={self.id}, name={self.name})"
+
+
+class Room(models.Model):
+	name = models.CharField(max_length=20)
+	venue = models.ForeignKey(Venue, on_delete=models.CASCADE)  #1
+
+	def __str__(self):
+		return f"Room(id={self.id}, name={self.name})"
