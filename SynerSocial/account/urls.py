ba410be3e5053945c3ac31ed852ab1fb961c4a6a
chwenjun225 <trantuan22052k@gmail.com>
2024-11-25 13:46:53 +0800

    Add user authentication views and update templates; implement login, logout, and password change functionality

--- SynerSocial/account/urls.py
+++ SynerSocial/account/urls.py
@@ -1,5 +1,23 @@
+from django.contrib.auth import views as auth_views
 from django.urls import path
 from . import views
+
 urlpatterns = [
-    path('login/', views.user_login, name='login'),
+	# previous login url
+	# path('login/', views.user_login, name='login'),
+	# login / logout urls
+	path('login/', auth_views.LoginView.as_view(), name='login'),
+	path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+	# change password urls
+	path(
+		'password-change/',
+		auth_views.PasswordChangeView.as_view(),
+		name='password_change'
+	),
+	path(
+		'password-change/done/',
+		auth_views.PasswordChangeDoneView.as_view(),
+		name='password_change_done'
+	),
+    path('', views.dashboard, name='dashboard'),
 ]
--- SynerSocial/account/views.py
+++ SynerSocial/account/views.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.decorators import login_required 
 from django.contrib.auth import authenticate, login
 from django.http import HttpResponse
 from django.shortcuts import render
@@ -25,3 +26,11 @@ def user_login(request):
 	else:
 		form = LoginForm()
 	return render(request, 'account/login.html', {'form': form})
+
+@login_required
+def dashboard(request):
+	return render(
+		request,
+		'account/dashboard.html',
+		{'section': 'dashboard'}
+	)
