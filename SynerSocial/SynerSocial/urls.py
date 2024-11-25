8950e95f0a07841d33fcc6ba9d3e74085e157d15
chwenjun225 <trantuan22052k@gmail.com>
2024-11-25 14:35:32 +0800

    Update authentication URLs and templates; refine login and logout functionality

--- SynerSocial/SynerSocial/urls.py
+++ SynerSocial/SynerSocial/urls.py
@@ -2,6 +2,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('account/', include('account.urls')),
+	path('admin/', admin.site.urls),
+	path('account/', include('account.urls')),
 ]
--- SynerSocial/account/forms.py
+++ SynerSocial/account/forms.py
@@ -2,4 +2,3 @@ from django import forms
 class LoginForm(forms.Form):
 	username = forms.CharField()
 	password = forms.CharField(widget=forms.PasswordInput)
-	
