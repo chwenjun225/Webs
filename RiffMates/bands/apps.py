0bf5b476001dd2219ba5993f6c7f82e59ddf2572
chwenjun225 <trantuan22052k@gmail.com>
2024-11-26 00:47:00 +0800

    Remove unused models, views, templates, and configuration files from home and bands apps

--- RiffMates/bands/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class BandsConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'bands'
--- RiffMates/bands/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-
-from bands import views
-
-urlpatterns = [
-	path('musician/<int:musician_id>/', views.musician, name="musician"),
-    path('musicians/', views.musicians, name="musicians"),
-]
