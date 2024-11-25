bf7fae5944b9868db811bf065cb3d6663d9f9ff3
chwenjun225 <trantuan22052k@gmail.com>
2024-11-26 00:03:38 +0800

    Add bands app and implement credits and news views; update settings and URLs

--- RiffMates/RiffMates/settings.py
+++ RiffMates/RiffMates/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'home.apps.HomeConfig', 
+    'bands.apps.BandsConfig'
 ]
 
 MIDDLEWARE = [
@@ -54,7 +56,7 @@ ROOT_URLCONF = 'RiffMates.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates', ], 
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- RiffMates/home/views.py
+++ RiffMates/home/views.py
@@ -1,3 +1,15 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+def credits(request):
+	content = "Nicky\nYour Name" 
+	return HttpResponse(content, content_type="text/plain")  
+
+def news(request):
+	data = {
+		'news':[
+			"RiffMates now has a news page!",
+			"RiffMates has its first web page",
+		],
+	}
+	return render(request, "news.html", data)
