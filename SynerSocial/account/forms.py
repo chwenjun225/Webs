f89f250089b8dcc1f118aa7e1d5df0cd2373c739
chwenjun225 <trantuan22052k@gmail.com>
2024-11-25 15:39:33 +0800

    Add user profile model and registration functionality; update settings, forms, and templates

--- SynerSocial/account/forms.py
+++ SynerSocial/account/forms.py
@@ -1,18 +1,20 @@
 from django import forms
 from django.contrib.auth import get_user_model
 
+from .models import Profile
+
 class LoginForm(forms.Form):
 	username = forms.CharField()
 	password = forms.CharField(widget=forms.PasswordInput)
 
 class UserRegistrationForm(forms.ModelForm):
 	password = forms.CharField(
-	label='Password',
-	widget=forms.PasswordInput
+		label='Password',
+		widget=forms.PasswordInput
 	)
 	password2 = forms.CharField(	
-	label='Repeat password',
-	widget=forms.PasswordInput
+		label='Repeat password',
+		widget=forms.PasswordInput
 	)
 	class Meta:
 		model = get_user_model()
@@ -22,3 +24,14 @@ class UserRegistrationForm(forms.ModelForm):
 		if cd['password'] != cd['password2']:
 			raise forms.ValidationError("Passwords don't match.")
 		return cd['password2']
+
+class UserEditForm(forms.ModelForm):
+	class Meta:
+		model = get_user_model()
+		fields = ['first_name', 'last_name', 'email']
+
+class ProfileEditForm(forms.ModelForm):
+	class Meta:
+		model = Profile
+		fields = ['date_of_birth', 'photo']
+		
--- SynerSocial/account/views.py
+++ SynerSocial/account/views.py
@@ -3,7 +3,8 @@ from django.contrib.auth import authenticate, login
 from django.http import HttpResponse
 from django.shortcuts import render
 
-from .forms import LoginForm
+from .forms import LoginForm, UserRegistrationForm
+from .models import Profile
 
 def user_login(request):
 	if request.method == 'POST':
@@ -33,4 +34,31 @@ def dashboard(request):
 		request,
 		'account/dashboard.html',
 		{'section': 'dashboard'}
+	)
+
+def register(request):
+	if request.method == 'POST':
+		user_form = UserRegistrationForm(request.POST)
+		if user_form.is_valid():
+			# Create a new user object but avoid saving it yet
+			new_user = user_form.save(commit=False)
+			# Set the chosen password
+			new_user.set_password(
+				user_form.cleaned_data['password']
+			)
+			# Save the User object
+			new_user.save()
+			# Create the user profile
+			Profile.objects.create(user=new_user)
+			return render(
+				request,
+				'account/register_done.html',
+				{'new_user': new_user}
+			)
+	else:
+		user_form = UserRegistrationForm()
+	return render(
+		request,
+		'account/register.html',
+		{'user_form': user_form}
 	)
