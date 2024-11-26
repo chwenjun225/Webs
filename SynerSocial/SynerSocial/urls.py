f6988b392f0d91b35ba59339dbc553ba3f00f49f
chwenjun225 <trantuan22052k@gmail.com>
2024-11-26 18:32:05 +0800

    Implement cart functionality with views for adding, removing, and displaying cart items; update URLs and templates for cart integration

--- SynerSocial/SynerSocial/urls.py
+++ SynerSocial/SynerSocial/urls.py
@@ -22,6 +22,7 @@ from django.urls import path, include
 
 urlpatterns = [
 	path('admin/', admin.site.urls),
+	path('cart/', include('cart.urls', namespace='cart')),
 	path('', include('shop.urls', namespace='shop')),
 ]
 
--- SynerSocial/shop/views.py
+++ SynerSocial/shop/views.py
@@ -1,3 +1,4 @@
+from cart.forms import CartAddProductForm
 from django.shortcuts import get_object_or_404, render
 from .models import Category, Product
 
@@ -19,11 +20,15 @@ def product_list(request, category_slug=None):
 	)
 
 def product_detail(request, id, slug):
-    product = get_object_or_404(
-        Product, id=id, slug=slug, available=True
-    )
-    return render(
-        request,
-        'shop/product/detail.html',
-        {'product': product}
-    )
+	product = get_object_or_404(
+		Product, id=id, slug=slug, available=True
+	)
+	cart_product_form = CartAddProductForm()
+	return render(
+		request,
+		'shop/product/detail.html',
+		{
+			'product': product, 
+			'cart_product_form': cart_product_form, 
+		}
+	)
