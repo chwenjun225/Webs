844229aa2e94afaabb821d870063821aaae818d3
chwenjun225 <trantuan22052k@gmail.com>
2024-11-28 00:31:55 +0800

    Add admin order detail view and template; implement URL routing for order management

--- SynerSocial/orders/views.py
+++ SynerSocial/orders/views.py
@@ -1,8 +1,10 @@
+from django.contrib.admin.views.decorators import staff_member_required
 from cart.cart import Cart
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from .forms import OrderCreateForm
-from .models import OrderItem
+from .models import OrderItem, Order
 from .tasks import order_created
+
 def order_create(request):
 	cart = Cart(request)
 	if request.method == 'POST':
@@ -30,3 +32,10 @@ def order_create(request):
 		'orders/order/create.html',
 		{'cart': cart, 'form': form}
 	)
+
+@staff_member_required
+def admin_order_detail(request, order_id):
+    order = get_object_or_404(Order, id=order_id)
+    return render(
+        request, 'admin/orders/order/detail.html', {'order': order}
+    )
