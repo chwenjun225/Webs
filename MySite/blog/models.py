e6ef3b57b97c0791b0d3e954fd4265951f2ec1fd
chwenjun225 <trantuan22052k@gmail.com>
2024-12-05 00:51:00 +0800

    Refactor blog post model and views; implement slug uniqueness for published date, add pagination to post list, and enhance URL routing for post detail view.

--- MySite/blog/models.py
+++ MySite/blog/models.py
@@ -1,5 +1,6 @@
 from django.conf import settings
 from django.db import models
+from django.urls import reverse 
 from django.utils import timezone
 class PublishedManager(models.Manager):
 	def get_queryset(self):
@@ -11,7 +12,11 @@ class Post(models.Model):
 		DRAFT = 'DF', 'Draft'
 		PUBLISHED = 'PB', 'Published'
 	title = models.CharField(max_length=250)
-	slug = models.SlugField(max_length=250)
+	slug = models.SlugField(
+		max_length=250, 
+		unique_for_date='publish', 
+		allow_unicode=True, 
+	)
 	author = models.ForeignKey(
 		settings.AUTH_USER_MODEL,
 		on_delete=models.CASCADE,
@@ -35,3 +40,13 @@ class Post(models.Model):
 		]
 	def __str__(self):
 		return self.title
+	def get_absolute_url(self):
+		return reverse(
+			'blog:post_detail',
+			args=[
+				self.publish.year,
+				self.publish.month,
+				self.publish.day,
+				self.slug, 
+			]
+		)
--- MySite/blog/views.py
+++ MySite/blog/views.py
@@ -1,21 +1,53 @@
+from django.core.paginator import (
+	Paginator, 
+	EmptyPage, 
+	PageNotAnInteger, 
+)
+from django.views.generic import ListView
 from django.shortcuts import render, get_object_or_404
 from django.http import Http404
 from .models import Post
+
 def post_list(request):
-	posts = Post.published.all()
+	post_list = Post.published.all()
+	# Pagination with 3 posts per page
+	paginator = Paginator(post_list, 3)
+	page_number = request.GET.get('page', 1)
+	try:
+		posts = paginator.page(page_number)
+	except PageNotAnInteger:
+		# If page_number is not an integer get the first page
+		posts = paginator.page(1)
+	except EmptyPage:
+		# If page_number is out of range get last page of results
+		posts = paginator.page(paginator.num_pages)
 	return render(
 		request,
-		'blog/post/list.html',
-		{'posts': posts}
+		'blog/post/list.html', 
+		{'posts': posts}, 
 	)
-def post_detail(request, id):
+
+def post_detail(request, year, month, day, post):
 	post = get_object_or_404(
 		Post,
-		id=id,
-		status=Post.Status.PUBLISHED
+		status=Post.Status.PUBLISHED,
+		slug=post,
+		publish__year=year,
+		publish__month=month,
+		publish__day=day, 
 	)
 	return render(
 		request,
-		'blog/post/detail.html',
-		{'post': post}
+		'blog/post/detail.html', 
+		{'post': post}, 
 	)
+
+class PostListView(ListView):
+	"""
+	Alternative post list view
+	"""
+	queryset = Post.published.all()
+	context_object_name = 'posts'
+	paginate_by = 3
+	template_name = 'blog/post/list.html'
+	
